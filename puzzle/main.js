faff9c83e146b98997b888362513f3b58b129460
Gerald Arthur Moore <gerald.moore42@gmail.com>
2025-04-12 05:55:25 +0200

    Test addWord function

--- puzzle/main.js
+++ puzzle/main.js
@@ -6,15 +6,28 @@ import { getGridAsObjects } from "./letterPositions.js";
 import { validIntersections } from "./validIntersections.js";
 import { placeWordAtPosition } from "./addWord.js";
 
-const rowSize = 10;
+const rowSize = 7;
 const letterArray = arrayGenerator(rowSize);
 generateGrid(letterArray);
 placeInitialWord(letterArray, rowSize, words[0], false, false);
 
 const gridObjects = getGridAsObjects(letterArray, rowSize);
-const validSpots = validIntersections("banana", gridObjects, rowSize);
+const validSpots = validIntersections("grape", gridObjects, rowSize);
 const chosenSpot = validSpots[0];
-placeWordAtPosition("banana", letterArray, rowSize, chosenSpot);
+placeWordAtPosition("grape", letterArray, rowSize, chosenSpot);
+
+const gridObjects_2 = getGridAsObjects(letterArray, rowSize);
+const validSpots_2 = validIntersections("cherry", gridObjects_2, rowSize);
+const chosenSpot_2 = validSpots_2[0];
+placeWordAtPosition("cherry", letterArray, rowSize, chosenSpot_2);
+
+const gridObjects_3 = getGridAsObjects(letterArray, rowSize);
+const validSpots_3 = validIntersections("egg", gridObjects_3, rowSize);
+const chosenSpot_3 = validSpots_3[0];
+placeWordAtPosition("egg", letterArray, rowSize, chosenSpot_3);
+
+
+
 
 generateGrid(letterArray);
 const updatedGridObjects = getGridAsObjects(letterArray, rowSize);
