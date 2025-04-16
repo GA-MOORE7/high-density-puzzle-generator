ef5cb1fe962e9a365f76829e4ed413cd6c798019
Gerald Arthur Moore <gerald.moore42@gmail.com>
2025-04-16 02:30:26 +0200

    Add feature: generate a single puzzle by iterating over the words array

--- puzzle/wordData.js
+++ puzzle/wordData.js
@@ -1,3 +1,12 @@
-export const words = ["apple", "banana", "cherry", "grape", "orange", "melon", "mango", "lemon"];
+export const words = [
+    "apple", "banana", "cherry", "grape", "orange", "melon", "mango", "lemon",
+    "kiwi", "peach", "pear", "plum", "fig", "date", "apricot", "blueberry",
+    "cantaloupe", "papaya", "pineapple", "watermelon", "raspberry", "blackberry",
+    "cranberry", "guava", "pomegranate", "lychee", "starfruit", "dragonfruit",
+    "passionfruit", "tangerine", "nectarine", "coconut", "lime", "boysenberry",
+    "persimmon", "currant", "gooseberry", "quince", "jackfruit", "durian",
+    "mulberry", "longan", "soursop", "rambutan", "blood orange", "ugli fruit",
+    "jabuticaba", "miracle fruit"
+  ];	
 
 
